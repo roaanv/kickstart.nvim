1014a1fea18f7befdecba479614e55c4b82e8a86
roaanv <roaanv@0112.io>
2025-02-09 16:47:26 +1100

    Removed clipboard

--- init.lua
+++ init.lua
@@ -116,7 +116,8 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  --RV  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = ''
 end)
 
 -- Enable break indent
