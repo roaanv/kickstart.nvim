06e2d0694ebf83144667b41c84bf16ba125cf4ed
roaanv <roaanv@0112.io>
2025-02-09 17:30:33 +1100

    Moved custom configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.opt.mouse = ''
+vim.opt.clipboard = ''
+vim.g.have_nerd_font = true
 return {}
